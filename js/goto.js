c53682b79192dfbd62e279ec7b6c358a3cc79af2
ZYallers <zyb_icanplay@163.com>
2018-06-11 16:33:12 +0800

    Update 取消延迟跳转；

--- js/goto.js
+++ js/goto.js
@@ -1,5 +1,8 @@
 (function () {
-    window.setTimeout(function(){
+    window.onload=function(){
         window.location.href = 'https://github.com/ZYallers/';
-    },1500);
+    };
+    /*window.setTimeout(function(){
+        window.location.href = 'https://github.com/ZYallers/';
+    },700);*/
 })();
