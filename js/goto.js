0bf6911d5748962c1df2368737da5cd39fd6f1b1
ZYallers <zyb_icanplay@163.com>
2019-10-26 15:17:54 +0800

    Update one second to direct;

--- js/goto.js
+++ js/goto.js
@@ -1,8 +1,8 @@
 (function () {
-    window.onload=function(){
+    /*window.onload=function(){
         window.location.href = 'https://github.com/ZYallers/';
-    };
-    /*window.setTimeout(function(){
+    };*/
+    window.setTimeout(function(){
         window.location.href = 'https://github.com/ZYallers/';
-    },700);*/
+    },1000);
 })();
